8cf22c01020ae1520c87162eeb9c293fc5114c4b
Douglas Gonçalves <60992128+douglas-ggoncalves@users.noreply.github.com>
2022-03-14 16:07:28 -0300

    .

--- projects/frontvue/src/views/Replic.vue
+++ projects/frontvue/src/views/Replic.vue
@@ -1,5 +1,43 @@
 <template>
   <div class="home">
+    <div class="wrapper">
+        <!-- Sidebar -->
+        <nav id="sidebar">
+            <ul class="list-unstyled components">
+                <div class="sidebar-header">
+                    <img class="img-fluid" src="../assets/img/logo-white.png">
+                    <hr>
+                </div>
+
+                <li>
+                    <a href="replicacao">Replicação</a>
+                </li>
+
+                <li>
+                    <a href="representante">Representante</a>
+                </li>
+
+                <li>
+                    <a href="#">Sobre</a>
+                </li>
+
+                <li>
+                    <a href="logout">Sair</a>
+                </li>
+            </ul>
+        </nav>
+
+        <div id="content">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="container-fluid">
+                    <button type="button" id="sidebarCollapse" class="btn btn-outline-dark" @click="clique()">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+            </nav>
+        </div>
+    </div>
+
     <h3>Home.vue</h3>
     <div v-if="err">
       <h3>{{ err }}</h3>
@@ -70,7 +108,127 @@ export default {
             Vue.set(this.data, y, err.response.data)
           }
         }
-    }
+    }, clique() {
+            var X = document.getElementById("sidebar").className;
+            if (X == "") {
+                document.getElementById("sidebar").className = "active"
+            } else {
+                document.getElementById("sidebar").className = ""
+            }
+        }
   }
 }
 </script>
+
+
+<style scoped>
+.wrapper {
+    display: flex;
+    width: 100%;
+    align-items: stretch;
+}
+
+#sidebar {
+    min-width: 250px;
+    max-width: 250px;
+    min-height: 100vh;
+}
+
+#sidebar.active {
+    margin-left: -250px;
+}
+
+a[data-toggle="collapse"] {
+    position: relative;
+}
+
+.dropdown-toggle::after {
+    display: block;
+    position: absolute;
+    top: 50%;
+    right: 20px;
+    transform: translateY(-50%);
+}
+
+@media (max-width: 768px) {
+    #sidebar {
+        margin-left: -250px;
+    }
+    #sidebar.active {
+        margin-left: 0;
+    }
+}
+
+@import "https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700";
+
+
+body {
+    font-family: 'Poppins', sans-serif;
+    background: #fafafa;
+}
+
+p {
+    font-family: 'Poppins', sans-serif;
+    font-size: 1.1em;
+    font-weight: 300;
+    line-height: 1.7em;
+    color: #999;
+}
+
+a, a:hover, a:focus {
+    color: inherit;
+    text-decoration: none;
+    transition: all 0.3s;
+}
+
+#sidebar {
+    background: linear-gradient(to bottom, #185992 0%, #31374F 100%);
+    color: #fff;
+    transition: all 0.3s;
+}
+
+#sidebar .sidebar-header {
+    padding: 20px;
+}
+
+#sidebar ul.components {
+    padding: 20px 0;
+    border-bottom: 1px solid #47748b;
+}
+
+#sidebar ul p {
+    color: #fff;
+    padding: 10px;
+}
+
+#sidebar ul li a {
+    padding: 10px;
+    font-size: 1.1em;
+    display: block;
+}
+#sidebar ul li a:hover {
+    color: #7386D5;
+    background: #fff;
+}
+/*
+#sidebar ul li.active > a, a[aria-expanded="true"] {
+    color: #fff;
+    background: #143450;
+}
+*/
+ul ul a {
+    font-size: 0.9em !important;
+    padding-left: 30px !important;
+    background: #225886;
+}
+
+.input-group .form-control{
+    border: 0;
+}
+
+button.btn.btn-link.aa:not(.collapsed){
+    background-color: #e7f1ff;
+    color: #0c63e4;
+}
+
+</style>
