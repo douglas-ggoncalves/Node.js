26c5818e4be4dd0fae8f18c66e35c79f16c87c1f
Douglas Gonçalves <60992128+douglas-ggoncalves@users.noreply.github.com>
2022-03-23 16:17:52 -0300

    .

--- projects/frontvue/src/views/Teste.vue
+++ projects/frontvue/src/views/Teste.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
+    <div class="example">
     <Multiselect
       v-model="example1.value"
       v-bind="example1"
     ></Multiselect>
+    </div>
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
+import '../assets/style/style.css'
+
 import VueCompositionAPI from '@vue/composition-api'
 import Multiselect from '@vueform/multiselect/dist/multiselect.vue2.js'
 Vue.use(VueCompositionAPI)
