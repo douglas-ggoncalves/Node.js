b1ca892be08773507995533d95639b15efc1f592
Douglas Gonçalves <60992128+douglas-ggoncalves@users.noreply.github.com>
2022-03-23 16:10:58 -0300

    .

--- projects/frontvue/src/views/Teste.vue
+++ projects/frontvue/src/views/Teste.vue
@@ -1,31 +1,42 @@
 <template>
-  <Multiselect
-    v-model="value"
-    :options="options"
-  />
+  <div id="app">
+    <Multiselect
+      v-model="example1.value"
+      v-bind="example1"
+    ></Multiselect>
+  </div>
 </template>
 
 <script>
-import Multiselect from '@vueform/multiselect'
-<style src="@vueform/multiselect/themes/default.css"></style>
+import Vue from 'vue'
+import VueCompositionAPI from '@vue/composition-api'
+import Multiselect from '@vueform/multiselect/dist/multiselect.vue2.js'
+Vue.use(VueCompositionAPI)
 
 export default {
   components: {
-    Multiselect,
-  },
-  data() {
-    return {
-      value: null,
-      options: [
-        'Vue',
-        'Script',
-        'Com',
-      ]
+    Multiselect
+    },
+  data(){
+    return{
+      
+      example1: {
+      value: 0,
+      options: ['Batman', 'Robin', 'Joker']
+    }
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
+.multiselect-green {
+  --ms-tag-bg: #D1FAE5;
+  --ms-tag-color: #059669;
+}
 
+.multiselect-blue {
+  --ms-tag-bg: #DBEAFE;
+  --ms-tag-color: #2563EB;
+}
 </style>
