5fb4be275293354e765c0fdd3f413cb3991019d6
Douglas Gonçalves <60992128+douglas-ggoncalves@users.noreply.github.com>
2022-05-01 21:43:03 -0300

    .

--- projects/backnode/database/database.js
+++ projects/backnode/database/database.js
@@ -1,12 +1,12 @@
 const knex = require('knex')({
-    client: 'mssql',
-    connection: {
-      host : 'localhost',
-      port : 3739,
-      user : 'sa',
-      password : 'd120588$788455',
-      database : 'MAXIMUS'
-    }
-  });
+  client: 'mssql',
+  connection: {
+    host : 'localhost',
+    port : 3739,
+    user : 'sa',
+    password : 'd120588$788455',
+    database : 'MAXIMUS'
+  }
+});
 
   module.exports = knex;
