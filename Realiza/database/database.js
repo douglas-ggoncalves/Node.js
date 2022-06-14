95cdf45f376c6830f0c98aed4fc19ae7fcbd9401
Douglas Gonçalves <60992128+douglas-ggoncalves@users.noreply.github.com>
2022-06-14 10:48:24 -0300

    Commit

--- Realiza/database/database.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require("sequelize");
-
-const connection = new Sequelize("realiza", "root", '93336848',{
-    host: 'localhost',
-    dialect: 'mysql',
-    timezone: "-03:00"
-});
-
-module.exports = connection;
